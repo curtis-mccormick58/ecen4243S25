55606856b2130b3a8793c5789b2aa7dbdee554f2
Benjamin Ashrafi <benjamin.ashrafi@okstate.edu>
2025-02-26 23:39:50 -0600

    fixed imem size to run /testing code

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -67,7 +67,7 @@ module testbench();
    initial
      begin
 	string memfilename;
-        memfilename = {"../riscvtest/lui.memfile"};
+        memfilename = {"../testing/add.memfile"};
         $readmemh(memfilename, dut.imem.RAM);
      end
 
@@ -320,7 +320,7 @@ endmodule // top
 module imem (input  logic [31:0] a,
 	     output logic [31:0] rd);
    
-   logic [31:0] 		 RAM[63:0];
+   logic [31:0] 		 RAM[416:0];
    
    assign rd = RAM[a[31:2]]; // word aligned
    
