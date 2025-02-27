f74d4dbac1fab124702be1ef1c5d0dc8973e8ecf
Benjamin Ashrafi <benjamin.ashrafi@okstate.edu>
2025-02-26 23:43:53 -0600

    added description to imem RAM

--- lab2/hdl/riscv_single.sv
+++ lab2/hdl/riscv_single.sv
@@ -320,7 +320,7 @@ endmodule // top
 module imem (input  logic [31:0] a,
 	     output logic [31:0] rd);
    
-   logic [31:0] 		 RAM[416:0];
+   logic [31:0] 		 RAM[416:0]; //change the amount of machine code instructions in .memfile so all instructions run
    
    assign rd = RAM[a[31:2]]; // word aligned
    
